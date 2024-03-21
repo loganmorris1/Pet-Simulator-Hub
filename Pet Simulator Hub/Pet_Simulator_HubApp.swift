d6ae1c11a0e12c408417b3c3047de592769af098
RCM Investment Holdings, LLC <rcminvestmentholdings@gmail.com>
2024-03-20 20:52:26 -0700

    Initial Commit

---
+++ Pet Simulator Hub/Pet_Simulator_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pet_Simulator_HubApp.swift
+//  Pet Simulator Hub
+//
+//  Created by Logan Morris on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Pet_Simulator_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
